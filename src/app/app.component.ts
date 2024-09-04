08a0554df84c0a7b09cf65bf65bd6bbf9c8ba7c0
NielzosFilms <nielzosfilms@gmail.com>
2024-09-04 17:59:44 +0200

    cell component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CellComponent } from './cell/cell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CellComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'game-of-life';
